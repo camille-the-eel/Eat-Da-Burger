0bdd181f5cf2deb06d4ae18552bb25795c5bb783
Camille <c.hughes64@gmail.com>
2019-03-06 12:49:02 -0500

    check

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,6 +3,7 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+    //RENDERING INDEX.HTML WITH DATABASE INFO 
     app.get("/", function (req, res) {
         db.Burger.findAll().then(function(data) {
             console.log(data);
